acab1a62ecf886800612262b02952e3f0a1335d8
SantamRC <santamdev404@gmail.com>
2022-12-09 23:44:50 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(PORT, function () {
+  console.log("listening on Port: " + PORT);
+});
